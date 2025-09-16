e5aafa074c5459581c370d1cbfd854d21d4d0e64
theme222 <sira.tongsima@yahoo.com>
2025-09-16 10:35:21 +0700

    Updated backend to not be so stupid

--- backend/models/resumeModel.js
+++ backend/models/resumeModel.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 
 const resumeSchema = new mongoose.Schema({
+  title: { type: String, default: 'Untitled Resume' },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   personalInfo: { type: Object, default: {} },
   summary: { type: String, default: '' },
